9622ec5c16e60143f9f80933977e9f8bbe31b57d
Ернар <yernar_grofild@Yernar-MacBook-Pro.local>
2025-10-27 22:40:36 +0500

    ssd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,19 @@
 function App() {
   return (
-    <div style={{
-      display: 'flex',
-      height: '100vh',
-      alignItems: 'center',
-      justifyContent: 'center',
-      flexDirection: 'column',
-      fontFamily: 'Arial, sans-serif',
-      background: '#0d1117',
-      color: '#61dafb'
-    }}>
-      <h1>Hello Beknur, Hello Dias! 🚀</h1>
-      <p>I deployed my firs web-page!</p>
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        fontFamily: "Arial, sans-serif",
+        background: "#0d1117",
+        color: "#61dafb",
+      }}
+    >
+      <h1>Hello Beknur, Hello Dias and Araylym! 🚀</h1>
+      <p>Thank you for your help!</p>
     </div>
   );
 }
